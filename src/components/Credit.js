11739e74afbaf12b998f81123fc3bb1640b40e19
ever-dev <everdev0923@gmail.com>
2024-07-16 04:31:07 -0400

    last commit after being done with my ecommerce website and my first time using git in a project

---
+++ src/components/Credit.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Credit() {
+    return (
+        <div className="credit">
+            <h3>Privacy Policy | Term and conditions</h3>
+            <p>© 2021 Ever Dev. All Rights Reserved</p>
+        </div>
+    )
+}
